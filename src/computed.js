b0f05ba8ec302790e25e2134cf9df1bd062c7d3e
Tim Hall <tim.hall.engr@gmail.com>
2017-04-03 20:43:10 -0400

    Opt-in for computed caching

--- src/computed.js
+++ src/computed.js
@@ -6,12 +6,13 @@ import {
   Events,
 } from 'backbone';
 
-export default function Computed(model, keys, fn) {
+export default function Computed(model, keys, fn, options) {
   let events;
   let has_cached = false;
   let cached;
 
   if (isFunction(keys)) {
+    options = fn;
     fn = keys;
     events = 'change';
   } else if (Array.isArray(keys)) {
@@ -20,16 +21,16 @@ export default function Computed(model, keys, fn) {
     events = keys.split(' ').map(key => `change:${key}`).join(' ');
   }
 
+  // For now, cache is opt-in
+  const should_cache = options && options.cache;
+
   // TODO Ran into issues when computed is updated, but bound in component initialize
   // Needs deeper fix with bindings in component.update to alleviate
   
   this.get = () => {
-    if (!has_cached) {
+    if (!has_cached || !should_cache) {
       cached = fn();
-
-      // Note: caching here is the goal,
-      // but had other issues due to updates outside of watched changes
-      // -> Only cache on recompute from model for now
+      has_cached = true;
     }
 
     return cached;
