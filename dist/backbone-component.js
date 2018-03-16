fe111f12b293fa55325aacc994d24113122e303e
Tim Hall <tim.hall.engr@gmail.com>
2018-03-15 23:58:32 -0400

    v0.5.0

--- dist/backbone-component.js
+++ dist/backbone-component.js
@@ -1,6 +1,6 @@
 /*!
  * backbone-component - Backbone + Handlebars components
- * v0.4.0 - https://github.com/CSNW/backbone-component - @license: MIT
+ * v0.5.0 - https://github.com/CSNW/backbone-component - @license: MIT
  */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('underscore'), require('backbone'), require('handlebars')) :
@@ -624,7 +624,7 @@ function resolved(value) {
   return new Resolved(value);
 }
 
-var version = "0.4.0";
+var version = "0.5.0";
 
 exports.Observable = Observable;
 exports.observable = observable;
